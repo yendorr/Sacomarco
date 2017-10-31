634948e2b34e5d68e6aa21dba12effec1ccc7c40
Rodney Renato de Souza Silva <161025021@ad.fc.unesp.br>
2017-10-30 23:01:38 -0200

    Calculando Cr

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
+const maxVariaveis = 12, minVariaveis = 1, maxRestricoes = 12, minRestricoes = 1;
+const M = (27+10+1998)*999999999999999999999999999, m = 999999999999999999999999999;
 var variaveis = 3,restricoes = 3;
 var max = false;
-var i,j;
-const maxVariaveis = 12, minVariaveis = 3, maxRestricoes = 12, minRestricoes = 3;
+var i,j,j;
 var b = [], Cr = [], A = [], funcaoObjetivo = [], s = [], ba = [];
-var basica = [], naoBasica = [],sobra = [], artificial = [];
+var basica = [], naoBasica = [],sobra = [], deOnde = [], artificial = [];
+var contadorBasicas, contadorSobras,contadorArtificiais;
 primeiraExecução = true;
-for(i=0;i<maxVariaveis*3;i++) A[i]=[];
+for(i=0;i<=maxVariaveis*3;i++) A[i]=[];
 
 $(document).ready(function(){
 	atulizaTabela();
@@ -82,7 +84,7 @@ function salvaDados(){
 		funcaoObjetivo[i] = $("#z"+i).val();
 		for(j=1;j<=variaveis;j++)
 			A[i][j] = $("#A"+i+j).val();
-		// s[i] = $("#s"+i" :selected").text();
+		s[i] = $("#s"+i).find('option:selected').text();
 		b[i] = $("#b"+i).val();
 	}
 }
@@ -94,35 +96,159 @@ function restauraDados(){
 			$("#A"+i+j).val(A[i][j]);
 		$("#b"+i).val(b[i]);
 	}	
+	if(s[1])
+		for(i=1;i<=restricoes;i++)
+			$("#s"+i).val(s[i]);
 }
 
 function go(){
 	salvaDados();
-	// resolve();
+	resolve();
 }
 
 function resolve(){
+	zeraContadores();
 	formaPadrao();
-	do{
 
-		// empilhaTabela();
-		
-
-	}while(continua()=='s');
+	calculaCr();
+	preview();
 }
 
 function continua(){
 
 }
 
-function formaPadrao(){
-	if(max)
-		for(i=1;i<=restricoes;i++)
-			funcaoObjetivo[i]*=-1;
-
-		
+function zeraContadores(){
+	contadorBasicas=0;
+	contadorSobras=0;
+	contadorArtificiais=0;
+	variaveis = $("#variaveis").val();
+	restricoes = $("#restricoes").val();
 
 }
+
+function formaPadrao(){
+	if(max)
+		for(j=1;j<=variaveis;i++)
+			funcaoObjetivo[j]*=-1;	
+
+	positivaB();	
+
+	for(i=1;i<=restricoes;i++){
+		if (s[i]=='<') 
+			formaMenor(i);
+		else if (s[i]=='=') 
+			formaIgual(i);
+		else if (s[i]=='>') 
+			formaMaior(i);		
+	}
+}
+
+function positivaB(){
+	for(i=1;i<=restricoes;i++)
+		if(b[i]<0){
+			b[i]*=-1;
+
+			if (s[i]=='>') 
+				s[i]='<';
+			else if(s[i]=='<')
+				s[i]='>';
+
+			for(j=1;j<=variaveis;j++)
+				A[i][j]*=-1
+		}
+}
+
+function formaMenor(linha){
+	variaveis++;	
+	for(j=1;j<=restricoes;j++)
+		A[j][variaveis] = 0;
+	basica[++contadorBasicas] = variaveis;
+	sobra[++contadorSobras] = variaveis;
+	deOnde[contadorSobras] = j;
+	A[linha][variaveis] = 1;
+	funcaoObjetivo[variaveis] = 0;
+}
+
+function formaMaior(linha){
+	variaveis++;	
+	for(j=1;j<=restricoes;j++)
+		A[j][variaveis]=0;
+	sobra[++contadorSobras] = variaveis;
+	deOnde[contadorSobras] = j;
+	A[linha][variaveis] = -1;	
+	funcaoObjetivo[variaveis] = 0;
+
+
+	variaveis++;
+	for(j=1;j<=restricoes;j++)
+		A[j][variaveis] = 0;
+	basica[++contadorBasicas] = variaveis;
+	artificial[++contadorArtificiais] = variaveis;
+	A[linha][variaveis] = 1;
+	funcaoObjetivo[variaveis] = M; 
+}
+
+function formaIgual(linha){
+	variaveis++;
+	for(j=1;j<=restricoes;j++)
+		A[j][variaveis] = 0;
+	basica[++contadorBasicas] = variaveis;
+	artificial[++contadorArtificiais] = variaveis;
+	A[linha][variaveis] = 1;
+	funcaoObjetivo[variaveis] = M;
+}
+
+function calculaCr(){
+	for(j=1;j<=variaveis;j++){
+		soma=0;
+		for(i=1;i<=restricoes;i++){
+			soma+=funcaoObjetivo[basica[i]]*A[i][j];
+		}
+		Cr[j]=funcaoObjetivo[j] - soma;
+	}
+}
+
+function preview(){
+	console.log(A);
+	console.log(s);
+	console.log(b);
+	console.log(funcaoObjetivo);
+	$("#preview").append("z = ");
+	console.log("var = "+variaveis);
+	console.log("res = "+restricoes);
+	for(j=1;j<=variaveis;j++){
+		if(funcaoObjetivo[j]>m)
+		$("#preview").append("M");	
+		else
+		$("#preview").append(funcaoObjetivo[j]);
+		$("#preview").append("X"+j+" + ");
+	}
+	$("#preview").append("<br><br>");
+
+	for(i=1;i<=restricoes;i++){
+		$("#preview").append(basica[i]+" |");
+		for(j=1;j<=variaveis;j++){
+			$("#preview").append(A[i][j]);
+			$("#preview").append("X"+j+" + ");
+		}
+		$("#preview").append(" = "+b[i]);
+		$("#preview").append("<br>");
+	}
+	$("#preview").append("<br>--------------------------------------------<br>");
+	for(j=1;j<=variaveis;j++){
+		if(Cr[j]>m||-m>Cr[j]){
+			if(Cr[j]>0)
+				$("#preview").append("M | ");		
+			else
+				$("#preview").append("-M | ");
+		}
+		else
+		$("#preview").append(Cr[j]+" | ");
+	}
+
+}
+
 function atualizaMax(){
 	$("#max").empty();
 	if(max)	$("#max").append("Minimizar");
